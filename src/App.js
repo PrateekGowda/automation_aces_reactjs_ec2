a7e621ba95eaa494f06386c7e1b842a99b71d1e9
Prateek T L <tlprateektl@gmail.com>
2025-05-15 09:52:24 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import RVToolsParser from './components/RVToolsParser';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>RVTools Excel Parser</h1>
+      </header>
+      <main>
+        <RVToolsParser />
+      </main>
+      <footer>
+        <p>Upload RVTools Excel exports, parse to JSON, and submit to AWS Lambda</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
